8003743e387d35ef9930e1f11b1a024f97161cc8
Travis Alstrand <travisalstrand@gmail.com>
2023-11-02 11:10:30 -0700

    adjust app's static path, linked css / favicon

--- modules/models.py
+++ modules/models.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///projects.db"
 db = SQLAlchemy(app)
 
