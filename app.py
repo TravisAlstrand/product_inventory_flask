d2738d4d238b06d67b8ee9c4ad2d80ad7d25ba8f
Travis Alstrand <travisalstrand@gmail.com>
2023-11-02 10:56:53 -0700

    routes / basic templates created

--- app.py
+++ app.py
@@ -4,9 +4,41 @@ from modules.models import db, app
 from modules.csv_to_db import add_csv_to_db
 
 
+# HOME
 @app.route("/")
 def index():
   return render_template("index.html")
+
+# PRODUCTS
+@app.route("/browse-products")
+def browse_products():
+  return render_template("browse-prods.html")
+
+@app.route("/search-products")
+def search_products():
+  return render_template("search-prods.html")
+
+@app.route("/add-product")
+def add_product():
+  return render_template("add-prod.html")
+
+# BRANDS
+@app.route("/browse-brands")
+def browse_brands():
+  return render_template("browse-brands.html")
+
+@app.route("/search-brands")
+def search_brands():
+  return render_template("search-brands.html")
+
+@app.route("/add-brand")
+def add_brand():
+  return render_template("add-brand.html")
+
+# 404
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
 
 if __name__ == "__main__":
   db_exists = exists("./instance/projects.db")
