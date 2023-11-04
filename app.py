01a900dfa434c5f4831b1146208670549be2f96e
Travis Alstrand <travisalstrand@gmail.com>
2023-11-04 09:50:27 -0700

    edit / delete links added, edit.html created

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from modules.handle_search import handle_search, get_single_product, get_single_
 def index():
   return render_template("index.html")
 
+
 # GET STARTED
 @app.route("/get-started", methods=["GET", "POST"])
 def get_started():
@@ -20,15 +21,16 @@ def get_started():
       return redirect(url_for("search_page", service=request.form["b_or_p"]))
   return render_template("get-started.html")
 
+
 # SEARCH PAGE
 @app.route("/search/<service>", methods=["GET", "POST"])
-def search_page(service=None):
+def search_page(service=None, results=None):
   search_query = None
-  results = None
   if request.form:
     search_query = request.form["search"]
     results = handle_search(service.capitalize(), search_query.lower())
   return render_template("search.html", service=service, query=search_query, results=results)
+
 
 # PRODUCT DETAIL PAGE
 @app.route("/product/<result>")
@@ -36,16 +38,19 @@ def product_detail(result):
   result = get_single_product(result)
   return render_template("prod-detail.html", product=result)
 
+
 # BRAND DETAIL PAGE
 @app.route("/brand/<result>")
 def brand_detail(result):
   brand_count = get_single_brand(result)
   return render_template("brand-detail.html", brand=brand_count[0], count=brand_count[1])
 
+
 # 404
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', msg=error), 404
+
 
 if __name__ == "__main__":
   db_exists = exists("./instance/projects.db")
